a3a2b16f0248fe9125584ec0eecf67f8424768d0
Michaël van de Giessen <git@tubby.nl>
2017-07-13 10:03:19 +0200

    tutorial step 1
    https://angular.io/tutorial/toh-pt1

--- src/main/ng/app/app.component.ts
+++ src/main/ng/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { HelloService } from './services/hello.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HelloService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
