2347d60b343bb5c84530dd57f1e82cbccf081a6a
Michaël van de Giessen <git@tubby.nl>
2017-07-13 12:44:20 +0200

    extracting hero-detail template and routing

--- src/main/ng/app/hero-detail/hero-detail.component.ts
+++ src/main/ng/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core'
 import { Hero } from '../hero'
 
 @Component({
-  selector: 'app-hero-detail',
+  selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero;
+  @Input() hero: Hero;
 
   constructor() { }
 
