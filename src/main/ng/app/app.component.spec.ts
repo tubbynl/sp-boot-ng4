00780d6cf9b7124e4d2614415057208c002b86bf
Michaël van de Giessen <git@tubby.nl>
2017-07-13 12:15:19 +0200

    listing of heroes
    https://angular.io/tutorial/toh-pt2

--- src/main/ng/app/app.component.spec.ts
+++ src/main/ng/app/app.component.spec.ts
@@ -17,10 +17,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Tour of Heroes'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Tour of Heroes');
   }));
 
   it('should render title in a h1 tag', async(() => {
