00facf8cc00a15262b39ce08fe59a6dc5ff7839a
Michaël van de Giessen <git@tubby.nl>
2017-07-13 12:59:06 +0200

    using hero.service for retrieving heroes list

--- src/main/ng/app/app.component.ts
+++ src/main/ng/app/app.component.ts
@@ -1,32 +1,30 @@
-import { Component } from '@angular/core'
+import { Component,OnInit } from '@angular/core'
 import { HelloService } from './services/hello.service'
 import { Hero } from './hero'
+import { HeroService } from './hero.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [HelloService]
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes = this.heroService.getHeroes();
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+}
