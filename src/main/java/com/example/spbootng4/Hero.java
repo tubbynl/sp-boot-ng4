61b2fcc460a64c9013c1a0bfd842883a55d47a03
Michaël van de Giessen <git@tubby.nl>
2017-07-13 15:43:47 +0200

    backend HeroController

---
+++ src/main/java/com/example/spbootng4/Hero.java
@@ -0,0 +1,29 @@
+package com.example.spbootng4;
+
+import java.time.LocalDateTime;
+
+public class Hero {
+  private Long id;
+  private String name;
+
+  public Hero(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/com/example/spbootng4/HeroController.java
@@ -0,0 +1,44 @@
+package com.example.spbootng4;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/hero")
+public class HeroController {
+
+  private List<Hero> heroes = new ArrayList<>(Arrays.asList(
+    new Hero(0l, "Zero"),
+    new Hero(11l, "Mr. Nice"),
+    new Hero(12l, "Narco"),
+    new Hero(13l, "Bombasto"),
+    new Hero(14l, "Celeritas"),
+    new Hero(15l, "Magneta"),
+    new Hero(16l, "RubberMan"),
+    new Hero(17l, "Dynama"),
+    new Hero(18l, "Dr IQ"),
+    new Hero(19l, "Magma"),
+    new Hero(20l, "Tornado")));
+
+  @GetMapping
+  @ResponseBody
+  public List<Hero> list() {
+    return this.heroes;
+  }
+
+  @GetMapping("/{id}")
+  @ResponseBody
+  public Hero get(@PathVariable("id") Long id) {
+    return this.heroes.stream().filter(h->h.getId().equals(id)).findAny().orElse(null);
+  }
+
+  @PostMapping("/{id}")
+  public Long save(@RequestBody Hero hero) {
+    Long id = this.heroes.stream().map(Hero::getId).max(Long::compareTo).orElse(0l)+1;
+    this.heroes.add(new Hero(id,hero.getName()));
+    return id;
+  }
+}
