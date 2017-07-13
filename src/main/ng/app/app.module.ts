491ccf0024b94e78b3da5785d07c8f457a273709
Michaël van de Giessen <git@tubby.nl>
2017-07-13 15:58:31 +0200

    replacing mock-heroes with http rest endpoint

--- src/main/ng/app/app.module.ts
+++ src/main/ng/app/app.module.ts
@@ -2,18 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
+import { HttpModule }    from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component'
 
-import { AppRoutingModule } from './app-routing.module';
-
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
