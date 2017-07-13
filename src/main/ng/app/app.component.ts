6fd81505d6ce17dd3078f6049978f77c82b51ff3
Michaël van de Giessen <git@tubby.nl>
2017-07-13 12:40:51 +0200

    ng generate class hero
    extracted Hero to own class
    https://angular.io/tutorial/toh-pt3

--- src/main/ng/app/app.component.ts
+++ src/main/ng/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { HelloService } from './services/hello.service';
+import { HelloService } from './services/hello.service'
+import { Hero } from './hero'
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,6 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
